5933eca0e4281802182334c9140711b48dea8186
Raisul Islam Hridoy <raisul15-3813@diu.edu.bd>
2023-11-21 00:35:30 +0600

    modify  dashboard

--- Index.js
+++ Index.js
@@ -25,11 +25,49 @@ async function run() {
       // Connect the client to the server	(optional starting in v4.7)
       await client.connect();
 
+      const userCollection = client.db('Bistro-Boss-Restaurant').collection('users');
       const menuCollection = client.db('Bistro-Boss-Restaurant').collection('menu');
       const reviewCollection = client.db('Bistro-Boss-Restaurant').collection('review');
       const cartCollection = client.db('Bistro-Boss-Restaurant').collection('cart');
 
 
+
+      // user related api
+      app.get('/users', async(req, res) =>{
+         const result = await userCollection.find().toArray();
+         res.send(result)
+      })
+      app.post('/users', async(req, res) =>{
+         const user = req.body;
+         // insert email if user doesn't exists.
+         //  you can do this many ways (1.email unique 2.upsert 3.simple checking)
+         const query = {email : user.email}
+         const existingUser = await userCollection.findOne(query);
+         if(existingUser){
+            return res.send({ message: 'user already exists', insertedId: null})
+         }
+
+         const result = await userCollection.insertOne(user);
+         res.send(result)
+      })
+      app.patch('/users/admin/:id', async (req, res) => {
+         const id = req.params.id;
+         const filter = { _id: new ObjectId(id) };
+         const updatedDoc = {
+           $set: {
+             role: 'admin'
+           }
+         }
+         const result = await userCollection.updateOne(filter, updatedDoc);
+         res.send(result);
+       })
+   
+       app.delete('/users/:id', async (req, res) => {
+         const id = req.params.id;
+         const query = { _id: new ObjectId(id) }
+         const result = await userCollection.deleteOne(query);
+         res.send(result);
+       })
       // get all menu
       app.get('/menu', async (req, res) => {
          const cursor = menuCollection.find();
@@ -51,7 +89,7 @@ async function run() {
          res.send(result)
       });
       // delete
-      app.delete('/card/:id', async(req, res) =>{
+      app.delete('/carts/:id', async(req, res) =>{
          const id = req.params.id;
          const query = { _id: new ObjectId(id)}
          const result = await cartCollection.deleteOne(query);
